4302ffbf3eafa84f896e9e888b8a233851b0f34e
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2022-11-07 21:04:58 -0300

    GreenBlog-Start
    Criado base Html e Css do GreenBlog

--- flexcourse/fintechblog/app.js
+++
@@ -1,8 +0,0 @@
-const burguer = document.getElementById('burguer');
-
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
-    
-}
-burguer .addEventListener('click', toggleMenu);
